2e43ed563d6e410b70a051ed0087ee549a5623a8
nilssoncjonas <nilssoncjonas@gmail.com>
2024-02-09 21:45:49 +0100

    Created main async func, launched browser, navigated to page and close browser

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { Browser } from "puppeteer"
+
+const puppeteer = require('puppeteer')
+
+const url = 'https://books.toscrape.com/'
+
+const main = async () => {
+
+	const browser: Browser = await puppeteer.launch()
+	console.log('browser open')
+
+	const page = await browser.newPage()
+
+	await page.goto(url)
+
+	await browser.close()
+	console.log('done!')
+}
+main()
