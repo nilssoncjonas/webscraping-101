3f5c72792d47c50045a8040b2d971b5ce4af514d
nilssoncjonas <nilssoncjonas@gmail.com>
2024-02-09 22:13:13 +0100

    Added convert functions to convert price and rating from string to number

--- index.ts
+++ index.ts
@@ -14,12 +14,24 @@ const main = async () => {
 	await page.goto(url)
 
 	const bookData = await page.evaluate((url) => {
+		const convertPrice = (price: string) => parseFloat(price.replace('£', ''))
+		const convertRating = (rating: string) => {
+			switch (rating) {
+				case 'One': return 1
+				case 'Two': return 2
+				case 'Three': return 3
+				case 'Four': return 4
+				case 'Five': return 5
+				default: return 0
+			}
+		}
+
 		const bookPods = Array.from(document.querySelectorAll('.product_pod'))
 		const data = bookPods.map((book: any) => ({
 			title: book.querySelector('h3 a').getAttribute('title'),
-			price: book.querySelector('.price_color').innerText,
+			price: convertPrice(book.querySelector('.price_color').innerText),
 			imgSrc: url + book.querySelector('img').getAttribute('src'),
-			rating: book.querySelector('.star-rating').classList[1]
+			rating: convertRating(book.querySelector('.star-rating').classList[1])
 		}))
 		return data
 	}, url)
